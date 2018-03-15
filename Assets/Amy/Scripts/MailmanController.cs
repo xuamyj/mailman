749eb03a2a01f5eff2a880c06ae5bd60ec4ecac7
xuamyj <xuamyj@stanford.edu>
2018-03-14 22:53:35 -0700

    jk bumpy terrain, camera follows yay

--- Assets/Amy/Scripts/MailmanController.cs
+++ Assets/Amy/Scripts/MailmanController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class MailmanController : MonoBehaviour {
 
@@ -8,15 +9,30 @@ public class MailmanController : MonoBehaviour {
 	private Dashboard dashboard;
 
 	private Rigidbody rb;
+	Camera mainCamera;
 
 	const float VELOCITY_CAP_FACTOR = 0.9f;
-	const float FORCE_MULT_FACTOR = 200.0f;
+	[SerializeField]
+	float FORCE_MULT_FACTOR = 20000.0f;
 	const float TURN_EASINESS_FACTOR = 8.0f;
+
+	[SerializeField]
+	float UP_ROT_ANGLE = 15.0f;
+	[SerializeField]
+	float RIGHT_ROT_ANGLE = 10.0f;
+	[SerializeField]
+	float UP_POS_MOVE = 16.6f;
+	[SerializeField]
+	float BACK_POS_MOVE = -51.2f;
+
+	float CAMERA_TWEEN_CONST = 0.5f;
 
 	// Use this for initialization
 	void Start () {
 		dashboard = GetComponent<Dashboard> ();
 		rb = GetComponent<Rigidbody> ();
+
+		mainCamera = Camera.main;
 	}
 
 	// https://answers.unity.com/questions/35541/problem-finding-relative-rotation-from-one-quatern.html
@@ -24,13 +40,14 @@ public class MailmanController : MonoBehaviour {
 	//	Quaternion getRelativeRotation(Quaternion a, Quaternion b) {
 	//		return Quaternion.Inverse (a) * b;
 	//	}
-
+	
 	void FixedUpdate() {
-		Vector3 force = new Vector3 (Input.GetAxis("Horizontal"), 
-			0.0f, Input.GetAxis("Vertical"));
-		if (force.magnitude > 0.1f) {
+		Vector3 force = this.transform.forward * Input.GetAxis ("Vertical") * FORCE_MULT_FACTOR * Time.fixedDeltaTime; 
+		Vector3 torque = this.transform.up * Input.GetAxis ("Horizontal") * FORCE_MULT_FACTOR * Time.fixedDeltaTime;
+		if (force.magnitude > 0.01f || torque.magnitude > 0.01f) {
 			// add force
-			rb.AddForce (force * FORCE_MULT_FACTOR);
+			rb.AddForce (force);
+			rb.AddTorque (torque);
 
 			// add "air resistance" (caps speed)
 			rb.velocity *= VELOCITY_CAP_FACTOR;
@@ -40,19 +57,44 @@ public class MailmanController : MonoBehaviour {
 			this.transform.eulerAngles = new Vector3(0.0f, yRot, 0.0f);
 
 			// apply torque toward proper direction
-			// Vector3 lookPos = new Vector3(rb.velocity.x, 0.0f, rb.velocity.z); face velocity
-			Vector3 lookPos = force;
-			var rotation = Quaternion.LookRotation (lookPos);
-			transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * TURN_EASINESS_FACTOR);
 			// THIS DID NOT WORK! but good try... rb.AddTorque(rotationToAdd.eulerAngles * 0.1f); 
+
+			// for old version where we face the way we're holding
+			// Vector3 lookPos = force;
+			// var rotation = Quaternion.LookRotation (lookPos);
+			// transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * TURN_EASINESS_FACTOR);
 		} else {
 			// TODO: need to add "screech" animation here
 			rb.velocity = Vector3.zero;
 		}
 	}
 
+	Vector3 rotateByAngle(Vector3 forwardVector, Vector3 axis, float angle) {
+		return Quaternion.AngleAxis (angle, axis) * forwardVector;
+	}
+
+	void updateCamera() {
+		// find truck vectors
+		Vector3 truckForward = this.transform.forward;
+		Vector3 truckRight = this.transform.right;
+		Vector3 truckUp = this.transform.up;
+
+		// find camera angle
+		Vector3 cameraForward = rotateByAngle (truckForward, truckUp, UP_ROT_ANGLE); 
+		cameraForward = rotateByAngle(cameraForward, truckRight, RIGHT_ROT_ANGLE);
+
+		// find camera position
+		Vector3 cameraPosition = this.transform.position + BACK_POS_MOVE * cameraForward;
+		cameraPosition.y = UP_POS_MOVE;
+
+		mainCamera.transform.DOMove(cameraPosition, CAMERA_TWEEN_CONST);
+		mainCamera.transform.DOLookAt (this.transform.position, CAMERA_TWEEN_CONST);
+	}
+
 	void Update()
 	{
+		updateCamera ();
+
 		// pick up mail
 		if (Input.GetKeyDown (KeyCode.RightShift)) {
 			if (currHouseTouching) {
