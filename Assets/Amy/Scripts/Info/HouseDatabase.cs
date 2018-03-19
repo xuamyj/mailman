7b550509faabc08917b9334adb988ebb0eab1cf7
xuamyj <xuamyj@stanford.edu>
2018-03-19 06:34:20 -0700

    made some changes, can't even keep track anymore sorry

--- Assets/Amy/Scripts/Info/HouseDatabase.cs
+++ Assets/Amy/Scripts/Info/HouseDatabase.cs
@@ -12,10 +12,24 @@ public class HouseDatabase : MonoBehaviour {
 	string[] allHouses = new[] { 
 		// hey! if you change this, change each house's 
 		// HouseController.houseID in unity as well!
-		"MP|1 Rose|Leaf Club Leader", 
-		"DD|2 Oak|Jamie", 
-		"ZZ|1 Lake|Sleepy Cat",
-		"QZ|???|Secret Admirer",
+		"FamilyDad|1 Moon St|Cameron", 
+		"2W|2 Windy Dr|2W_name", 
+		"3W|3 Windy Dr|3W_name",
+		"Lonely|4 Windy Dr|Iman",
+		"1H|1 Hyde Way|1H_name",
+		"KidMove|2 Hyde Way|Saranya",
+		"3H|3 Hyde Way|3H_name",
+		"4H|4 Hyde Way|4H_name",
+		"KidStay|5 Hyde Way|Addie",
+		"1P|1 Pond Ave|1P_name",
+		"CrushSecret|2 Pond Ave|Toni",
+		"3P|3 Pond Ave|3P_name",
+		"KidNew|4 Pond Ave|4P_name",//"KidNew|4 Pond Ave|Saranya"
+		"1L|1 Leaf St|1L_name",
+		"2L|2 Leaf St|2L_name",
+		"CrushReceive|3 Leaf St|Jeff",
+		"FamilyGirl|4 Leaf St|Cassie",
+		"5L|5 Leaf St|5L_name",
 	};
 
 	public House getHouseByID(string houseID) {
--- Assets/Amy/Scripts/Info/MailDatabase.cs
+++ Assets/Amy/Scripts/Info/MailDatabase.cs
@@ -9,14 +9,20 @@ public class MailDatabase : MonoBehaviour {
 	// database
 	private Dictionary<string, Mail> db = new Dictionary<string, Mail>();
 
-	// mailID | fromHouseID | toHouseID \ daysToRead | text
+	// mailID | fromHouseID | toHouseID \ daysToRead | startDay | text
+	//string[] allMail = new[] {
+	//	"1001|KidMove|KidStay|1|1|Dear Addie,\n\nI'm so, so, SO sorry I broke your Tamagotchi. I'm the worst Best Best Friend ever!",
+	//	"5001|1H|3H|10|1|Hey, thanks for letting me borrow your lawnmower.",
+	//	"6001|4H|1H|10|1|I found a great log in the woods.\n\nWhen you get a chance, you should come see it.",
+	//	"1002|KidStay|KidMove|1|X|Dear Saranya,\n\nAw, it's OK! Just let me play with yours whenever I want.",
+	//	"2001|KidMove|CrushSecret|2|2|Dear Toni,\n\nHave a mice birthday! Mouse picture below. You're the best babysitter ever!"
+	//};
 	string[] allMail = new[] {
-		"1000|MP|DD|2|secret admirer letter",
-		"1001|DD|QZ|1|Dear Secret Admirer,\n\nThe poems are nice, but tell me more about you.",
-		"1002|MP|DD|1|response 2/part 2",
-		"1003|DD|MP|3|response 3",
-		"1004|MP|DD|1|response 4",
-		"2001|MP|ZZ|1|other house blah"
+		"1001|KidMove|KidStay|1|Dear Addie,\n\nI'm so, so, SO sorry I broke your Tamagotchi. I'm the worst Best Best Friend ever!",
+		"5001|1H|3H|10|Hey, thanks for letting me borrow your lawnmower.",
+		"6001|4H|1H|10|I found a great log in the woods.\n\nWhen you get a chance, you should come see it.",
+		"1002|KidStay|KidMove|1|Dear Saranya,\n\nAw, it's OK! Just let me play with yours whenever I want.",
+		"2001|KidMove|CrushSecret|2|Dear Toni,\n\nHave a mice birthday! Mouse picture below. You're the best babysitter ever!"
 	};
 
 	private HashSet<int> allStories = new HashSet<int> ();
@@ -45,8 +51,13 @@ public class MailDatabase : MonoBehaviour {
 			// .daysToRead
 			int daysToRead = Int32.Parse (entries [3]);
 
+			// .startDay
+			// uncomment and update Mail, TimeKeeper, etc. when not so sleepy
+			//int startDay = Int32.Parse (entries [4]);
+
 			// .text
 			string[] textArr = entries [4].Split('/');
+			//string[] textArr = entries [5].Split('/');
 			List<string> text = new List<string>();
 			for (int m = 0; m < textArr.Length; m++) {
 				text.Add(textArr[m]);
@@ -54,7 +65,8 @@ public class MailDatabase : MonoBehaviour {
 
 			// actually populate
 			Mail mail = new Mail ();
-			mail.initialize(mailID, entries [1], entries [2], daysToRead, text);
+						mail.initialize(mailID, entries [1], entries [2], daysToRead, text);
+			//mail.initialize(mailID, entries [1], entries [2], daysToRead, startDay, text);
 			db [mailID] = mail;
 		}
 	}
